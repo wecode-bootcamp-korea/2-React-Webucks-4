a44d14088e975ad6e0e2d0d7252bd45d68e2992c
wonkooklee <const.wonkook@gmail.com>
2021-09-02 01:31:29 +0900

    Modified: [Mission 5] 댓글창 모듈화 진행 스타일 추가

--- src/pages/wonkookLee/Detail/components/ReviewThread.js
+++ src/pages/wonkookLee/Detail/components/ReviewThread.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './ReviewThread.scss';
 
 class ReviewThread extends Component {
   render() {
---
+++ src/pages/wonkookLee/Detail/components/commentData.js
@@ -0,0 +1,18 @@
+export const Comments = [
+  {
+    commentId: 1,
+    userId: 'coffee_lover',
+    userComment: '너무 맛있어요!',
+  },
+  {
+    commentId: 2,
+    userId: 'CHOCO7',
+    userComment: '오늘도 제주 비자림 콜드 브루를 마시러 제주도에 갑니다.',
+  },
+  {
+    commentId: 3,
+    userId: 'legend_dev',
+    userComment:
+      '진짜 제주 비자림 콜드 브루는 전설이다. 진짜 제주 비자림 콜드 브루는 전설이다. 진짜 제주 비자림 콜드 브루는 전설이다.',
+  },
+];

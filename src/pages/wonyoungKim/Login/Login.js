02a1d64a8a702442eb13abdab3dc00ada1404ee7
wonyoung <ablack1029@gmail.com>
2021-09-06 03:26:05 +0900

    mission 2-3

--- src/pages/wonyoungKim/Login/Login.js
+++ src/pages/wonyoungKim/Login/Login.js
@@ -1,21 +1,77 @@
 import React, { Component } from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import './Login.scss';
 
 class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      userName: '',
+      userPW: '',
+      idCheck: false,
+      pwCheck: false,
+      btnColor: '#c4e1fb',
+    };
+  }
+
+  //아이디 체크
+  handleIdInput = event => {
+    this.setState({ userName: event.target.value });
+    if (event.target.value.includes('@')) {
+      this.setState({ idCheck: true });
+    }
+  };
+
+  //비번 체크
+  handlePwInput = event => {
+    this.setState({ userPW: event.target.value });
+    if (event.target.value.length >= 5) {
+      this.setState({ pwCheck: true }, () => this.btnChangeColor());
+    }
+  };
+
+  //버튼색변화
+  btnChangeColor = () => {
+    if (this.state.idCheck && this.state.pwCheck) {
+      this.setState({ btnColor: '#1a6fb9' });
+    }
+  };
+
+  //아이디비번 입력확인
+  btnClick = () => {
+    console.log('ID :', this.state.userName);
+    console.log('pw:', this.state.userPW);
+  };
+
   render() {
     return (
-      <div id="loginPage">
-        <img src="/images/logo.PNG" className="logo" alt="webucks" />
-        <div className="loginBox">
-          <input className="userName" type="text" placeholder="전화번호,사용자 이름 또는 이메일" /> 
-          <input className="passWord" type="password" placeholder="비밀번호" /> 
-          <Link to="/List">
-            <button id="loginBtn">로그인</button>
+      <div id='loginPage'>
+        <img src='/images/logo.PNG' className='logo' alt='webucks' />
+        <div className='loginBox'>
+          <input
+            className='userName'
+            type='text'
+            placeholder='전화번호,사용자 이름 또는 이메일'
+            onChange={this.handleIdInput}
+          />
+          <input
+            className='userPW'
+            type='password'
+            placeholder='비밀번호'
+            onChange={this.handlePwInput}
+          />
+          <Link to='/List-wonyoungKim'>
+            <button
+              className='loginBtn'
+              style={{ backgroundColor: this.state.btnColor }}
+              onClick={this.btnClick}
+            >
+              로그인
+            </button>
           </Link>
         </div>
-        <a href="#!">비밀번호를 잊으셨나요?</a>
-        <script src="js/login.js"></script>
+        <a href='#!'>비밀번호를 잊으셨나요?</a>
+        <script src='js/login.js'></script>
       </div>
     );
   }

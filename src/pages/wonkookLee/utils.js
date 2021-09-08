bada176b302e68ea5578fe8e1fb65c17bbbc0673
wonkooklee <const.wonkook@gmail.com>
2021-09-08 12:31:16 +0900

    Removed: 불필요한 util 내 기능 제거

--- src/pages/wonkookLee/utils.js
+++ src/pages/wonkookLee/utils.js
@@ -1,25 +1,6 @@
 import { PureComponent } from 'react';
 import { withRouter } from 'react-router-dom';
 
-export function addLoginBodyStyle() {
-  document.body.classList.add('Login_body');
-}
-export function removeLoginBodyStyle() {
-  document.body.classList.remove('Login_body');
-}
-export function addListBodyStyle() {
-  document.body.classList.add('List_body');
-}
-export function removeListBodyStyle() {
-  document.body.classList.remove('List_body');
-}
-export function addDetailBodyStyle() {
-  document.body.classList.add('Detail_body');
-}
-export function removeDetailBodyStyle() {
-  document.body.classList.remove('Detail_body');
-}
-
 class ScrollIntoView extends PureComponent {
   componentDidMount = () => window.scrollTo(0, 0);
 
